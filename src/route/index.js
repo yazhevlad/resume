884c60f6a69c155b3cddfcda44ec1fd6388f1c28
unknown <vladsakovich00@gmail.com>
2023-06-03 18:00:18 -0400

    bootstrap 2 task 2

--- src/route/index.js
+++ src/route/index.js
@@ -370,8 +370,6 @@ router.get('/person', function (req, res) {
 
 // ================================================================
 
-// ================================================================
-
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/bio', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
@@ -701,14 +699,77 @@ router.get('/web', function (req, res) {
 
 // ================================================================
 
-// ================================================================
-
 router.get('/bootstrap', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('bootstrap', {
     layout: 'bootstrap',
   })
 })
+
+// ================================================================
+
+router.get('/task22', function (req, res) {
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('task22', {
+    page: {
+      title: 'Web',
+    },
+
+    layout: 'basic',
+
+    page_header: {
+      title: 'InnovaMind',
+      description: 'Welcome to our wonderful community',
+    },
+    section: {
+      about_us: {
+        title_list: {
+          main: 'InnovaMind',
+          sub: 'InnovaMind is a forward-thinking startup focused on innovation. We are dedicated to creating efficient and powerful software through the use of first-class functions.',
+        },
+        button: { text: 'Show more' },
+      },
+      advantage: {
+        title: 'Advantage',
+        efficiency: {
+          title: 'Efficiency',
+          sub_title: 'Streamline your software development',
+          description:
+            'Streamline your software development with our innovative platform...',
+          button: 'Show more',
+        },
+        innovation: {
+          title: 'Innovation',
+          sub_title:
+            'Forward-thinking approach to programming',
+          description:
+            ' Stay ahead of the curve with our forward-thinking approach to programming...',
+          button: 'Show more',
+        },
+        collaboration: {
+          title: 'Collaboration',
+          sub_title: 'Connect with like-minded developers',
+          description:
+            'Connect with a community of like-minded developers to share knowledge and insights...',
+          button: 'Show more',
+        },
+      },
+      agreement: {
+        title: 'Terms of use',
+        info: {
+          sub_title: 'Check out our requirements',
+          detail: {
+            text: 'By using or accessing the service, you agree to be bound by the terms and conditions outlined in the agreement.',
+            button: 'Аccept',
+          },
+        },
+      },
+    },
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
 
 // Підключаємо роутер до бек-енду
 module.exports = router
